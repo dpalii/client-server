ae49707b8643f5480259f49bcd2e6a180efc271d
Dmytro Palii <dpalii.study@gmail.com>
2023-05-29 16:00:10 +0300

    code

--- client/Program.cs
+++ client/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Client Program
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        string serverIP = "localhost";
+        int serverPort = 8888;
+
+        TcpClient client = new TcpClient();
+        client.Connect(serverIP, serverPort);
+
+        Console.WriteLine("Connected to server!");
+
+        NetworkStream stream = client.GetStream();
+
+        while (true)
+        {
+            Console.WriteLine("Enter a request to send to the server:");
+            string? request = Console.ReadLine();
+
+            if (request == null)
+            {
+                continue;
+            }
+
+            byte[] requestData = Encoding.ASCII.GetBytes(request);
+            stream.Write(requestData, 0, requestData.Length);
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+            Console.WriteLine("Server response: " + response);
+        }
+    }
+}
--- server/Program.cs
+++ server/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Server Program
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main()
+    {
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        int port = 8888;
+
+        TcpListener listener = new TcpListener(ipAddress, port);
+        listener.Start();
+        Console.WriteLine("Server started and listening for incoming connections...");
+
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Client connected!");
+
+            // Handle client connection in a separate thread
+            System.Threading.Tasks.Task.Run(() => HandleClient(client));
+        }
+    }
+
+    static void HandleClient(TcpClient client)
+    {
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+
+        while (true)
+        {
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string request = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received request from client: " + request);
+
+            string response = ProcessRequest(request);
+
+            byte[] responseData = Encoding.ASCII.GetBytes(response);
+            stream.Write(responseData, 0, responseData.Length);
+            Console.WriteLine("Sent response to client: " + response);
+        }
+    }
+
+    static string ProcessRequest(string request)
+    {
+        // Process the request and return a response
+        return "Hello, client! Your request was: " + request;
+    }
+}
