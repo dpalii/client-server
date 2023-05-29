3748a89d3dc15be3fb4e12443aa945a5f96cfccc
Dmytro Palii <dpalii.study@gmail.com>
2023-05-29 16:39:47 +0300

    improved testability

--- client/Program.cs
+++ client/Program.cs
@@ -3,38 +3,73 @@ using System;
 using System.Net.Sockets;
 using System.Text;
 
-class Client
+public class Client
 {
-    static void Main()
+    private readonly ITcpClient _tcpClient;
+
+    public Client(ITcpClient tcpClient, string serverIP, int serverPort)
+    {
+        _tcpClient = tcpClient;
+        _tcpClient.Connect(serverIP, serverPort);
+        Console.WriteLine("Connected to server!");
+    }
+
+    public string SendMessageToServer(string request)
+    {
+        NetworkStream stream = _tcpClient.GetStream();
+
+        byte[] requestData = Encoding.ASCII.GetBytes(request);
+        stream.Write(requestData, 0, requestData.Length);
+
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+        return response;
+    }
+
+    public static void Main()
     {
         string serverIP = "localhost";
         int serverPort = 8888;
 
-        TcpClient client = new TcpClient();
-        client.Connect(serverIP, serverPort);
+        ITcpClient tcpClient = new TcpClientWrapper();
+        Client client = new Client(tcpClient, serverIP, serverPort);
 
-        Console.WriteLine("Connected to server!");
-
-        NetworkStream stream = client.GetStream();
-
-        while (true)
+        while(true)
         {
-            Console.WriteLine("Enter a request to send to the server:");
+            Console.WriteLine("Enter your request:");
             string? request = Console.ReadLine();
+            if (request == null) break;
+            string response = client.SendMessageToServer(request);
 
-            if (request == null)
-            {
-                continue;
-            }
-
-            byte[] requestData = Encoding.ASCII.GetBytes(request);
-            stream.Write(requestData, 0, requestData.Length);
-
-            byte[] buffer = new byte[1024];
-            int bytesRead = stream.Read(buffer, 0, buffer.Length);
-            string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-
-            Console.WriteLine("Server response: " + response);
+            Console.WriteLine(response);
         }
+    }
+}
+
+public interface ITcpClient
+{
+    void Connect(string serverIP, int serverPort);
+    NetworkStream GetStream();
+}
+
+public class TcpClientWrapper : ITcpClient
+{
+    private readonly TcpClient _tcpClient;
+
+    public TcpClientWrapper()
+    {
+        _tcpClient = new TcpClient();
+    }
+
+    public void Connect(string serverIP, int serverPort)
+    {
+        _tcpClient.Connect(serverIP, serverPort);
+    }
+
+    public NetworkStream GetStream()
+    {
+        return _tcpClient.GetStream();
     }
 }
--- server/Program.cs
+++ server/Program.cs
@@ -4,20 +4,25 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-class Server
+public class Server
 {
-    static void Main()
-    {
-        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
-        int port = 8888;
+    private readonly ITcpListener _listener;
+    private readonly IRequestProcessor _requestProcessor;
 
-        TcpListener listener = new TcpListener(ipAddress, port);
-        listener.Start();
+    public Server(ITcpListener listener, IRequestProcessor requestProcessor)
+    {
+        _listener = listener;
+        _requestProcessor = requestProcessor;
+    }
+
+    public void Start()
+    {
+        _listener.Start();
         Console.WriteLine("Server started and listening for incoming connections...");
 
         while (true)
         {
-            TcpClient client = listener.AcceptTcpClient();
+            TcpClient client = _listener.AcceptTcpClient();
             Console.WriteLine("Client connected!");
 
             // Handle client connection in a separate thread
@@ -25,7 +30,7 @@ class Server
         }
     }
 
-    static void HandleClient(TcpClient client)
+    private void HandleClient(TcpClient client)
     {
         NetworkStream stream = client.GetStream();
         byte[] buffer = new byte[1024];
@@ -36,7 +41,7 @@ class Server
             string request = Encoding.ASCII.GetString(buffer, 0, bytesRead);
             Console.WriteLine("Received request from client: " + request);
 
-            string response = ProcessRequest(request);
+            string response = _requestProcessor.ProcessRequest(request);
 
             byte[] responseData = Encoding.ASCII.GetBytes(response);
             stream.Write(responseData, 0, responseData.Length);
@@ -44,7 +49,51 @@ class Server
         }
     }
 
-    static string ProcessRequest(string request)
+    public static void Main()
+    {
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        int port = 8888;
+        ITcpListener listener = new TcpListenerWrapper(ipAddress, port);
+        RequestProcessor requestProcessor = new RequestProcessor();
+        Server server = new Server(listener, requestProcessor);
+        server.Start();
+    }
+}
+
+public interface ITcpListener
+{
+    void Start();
+    TcpClient AcceptTcpClient();
+}
+
+public interface IRequestProcessor
+{
+    string ProcessRequest(string request);
+}
+
+public class TcpListenerWrapper : ITcpListener
+{
+    private readonly TcpListener _tcpListener;
+
+    public TcpListenerWrapper(IPAddress ipAddress, int port)
+    {
+        _tcpListener = new TcpListener(ipAddress, port);
+    }
+
+    public void Start()
+    {
+        _tcpListener.Start();
+    }
+
+    public TcpClient AcceptTcpClient()
+    {
+        return _tcpListener.AcceptTcpClient();
+    }
+}
+
+public class RequestProcessor : IRequestProcessor
+{
+    public string ProcessRequest(string request)
     {
         // Process the request and return a response
         return "Hello, client! Your request was: " + request;
